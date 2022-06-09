73bd1799396f6b67efd9826ef39ab07c595db691
Nguyễn Văn Phú Nhàn <pnnguyen9999@outlook.com.vn>
2022-06-09 12:26:53 +0700

    change rpc to ankr

--- src/config/dev.config.js
+++ src/config/dev.config.js
@@ -2,7 +2,7 @@ module.exports = {
     BLOCKCHAIN_NETWORK: "TESTNET",
     CHAIN_ID: "97",
     BSC_SCAN: "https://testnet.bscscan.com/tx/",
-    RPC_BSC: "https://data-seed-prebsc-1-s1.binance.org:8545/2",
+    RPC_BSC: "https://apis.ankr.com/cf4f5b58a11b4a9985b165c4df285c59/124be7463da088020b6d34893ac3c58a/binance/full/test",
     LOTTERY_CONTRACT: "0x3E13b4004eaCd15502F709b25ef5cc0d7DeD97d0",
     HEGEM_ADDRESS: "0x51B25339997152E735c5Eedd7474724199B23eeA",
 };
